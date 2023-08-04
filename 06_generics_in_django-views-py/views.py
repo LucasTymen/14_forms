b8f0bf368fad690bd78e68438a87f4135cfada98
Lucas Tymen <lucas.tymen@gmail.com>
2023-08-04 10:05:36 +0200

    06_generics_in_django-views-py

--- 06_generics_in_django-views-py/views.py
+++ 06_generics_in_django-views-py/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Owner
 # Add your imports below:
-
+from django.views.generic.edit import CreateView
+from .forms import OwnerCreateForm
 
 def home(request):
    context = {"name": "Djangover"}
@@ -12,3 +13,7 @@ class OwnerList(ListView):
    model = Owner
 
 # Add your class below:
+class OwnerCreate(CreateView):
+  model = Owner
+  template_name = "vetoffice/owner_create_form.html"
+  form_class = OwnerCreateForm
